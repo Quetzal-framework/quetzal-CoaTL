01ffcdea6477f681112c9c59d405d7838f3a1fd6
Arnaud Becheler <Arnaud.Becheler@egce.cnrs-gif.fr>
2017-06-20 10:39:06 +0200

    include file for coalescence module

---
+++ coalescence/coalescence.h
@@ -0,0 +1,19 @@
+// Copyright 2016 Arnaud Becheler    <Arnaud.Becheler@egce.cnrs-gif.fr>
+
+/***********************************************************************                                                                         *
+* This program is free software; you can redistribute it and/or modify *
+* it under the terms of the GNU General Public License as published by *
+* the Free Software Foundation; either version 2 of the License, or    *
+* (at your option) any later version.                                  *
+*                                                                      *
+***************************************************************************/
+
+// Header file including all coalescence module
+
+#include "merger.h"
+
+#include "utils/Tree.h"
+
+#include "utils/tree_creation_policies.h"
+
+#include "utils/Forest.h"
