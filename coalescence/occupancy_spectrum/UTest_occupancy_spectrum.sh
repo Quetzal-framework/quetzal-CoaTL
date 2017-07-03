ebc4b26566fc005e6e76591930fad5d43914fc69
Arnaud Becheler <Arnaud.Becheler@egce.cnrs-gif.fr>
2017-07-03 14:46:14 +0200

    example tests for Generator

--- coalescence/occupancy_spectrum/UTest_occupancy_spectrum.sh
+++ coalescence/occupancy_spectrum/UTest_occupancy_spectrum.sh
@@ -2,28 +2,10 @@
 
 echo -e "UTest_occupancy_spectrum\n|"
 
+cd test/Generator/
+
 echo -e "|--\t Generator_test..................................."
-g++ -o Utest -Wall -std=c++14 Generator_test.cpp
-./Utest
-rm Utest
-echo -e "|--\t DONE \n|"
-
-echo -e "|--\t OccupancySpectrumDistribution_test..................................."
-g++ -o Utest -Wall -std=c++14 OccupancySpectrumDistribution_test.cpp
-./Utest
-rm Utest
-echo -e "|--\t DONE \n|"
-
-echo -e "|--\t memoize_test..................................."
-g++ -o memoize_test -Wall -std=c++14 memoize_test.cpp
-./memoize_test
-rm memoize_test
-echo -e "|--\t DONE \n|"
-
-echo -e "|--\t spectrum_creation_policy_test..................................."
-g++ -o spectrum_creation_policy_test -Wall -std=c++14 spectrum_creation_policy_test.cpp
-./spectrum_creation_policy_test
-rm spectrum_creation_policy_test
+./UTest.sh
 echo -e "|--\t DONE \n|"
 
 echo -e "DONE\n"
--- coalescence/occupancy_spectrum/test/Generator/Generator_test.cpp
+++ coalescence/occupancy_spectrum/test/Generator/Generator_test.cpp
@@ -8,32 +8,27 @@
 *                                                                      *
 ***************************************************************************/
 
-/**
- * \file Generator_test.cpp
- * \brief Program for unit testing
- * \author Arnaud Becheler <Arnaud.Becheler@egce.cnrs-gif.fr>
- *
- * Test program for interface of occupancy spectrum generative algorithm.
- * Compiles with g++ -o test Generator_test.cpp -std=c++14 -Wall
- *
- */
 
-#include "Generator.h"
+#include "../../Generator.h"
+
+//! [Example]
 
 #include <iostream>
-#include <algorithm> // for copy
-#include <iterator> // for ostream_iterator
+#include <algorithm> // std::copy
+#include <iterator>  // std::ostream_iterator
 
 int main(){
-  using generator_type = quetzal::coalescence::occupancy_spectrum::Generator;
-  using spectrum_type = generator_type::occupancy_spectrum_type;
+  using quetzal::coalescence::occupancy_spectrum::Generator;
+  using spectrum_type = Generator::occupancy_spectrum_type;
 
-  auto spectrum_handler = [](spectrum_type const& M_j){
+  auto spectrum_handler = [](spectrum_type && M_j){
     std::copy(M_j.begin(), M_j.end(), std::ostream_iterator<spectrum_type::value_type>(std::cout, " "));
     std::cout << "\n";
   };
 
-  generator_type::generate(5,10, spectrum_handler);
+  Generator::generate(5, 10, spectrum_handler);
 
   return 0;
 }
+
+//! [Example]
