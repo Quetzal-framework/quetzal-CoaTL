9f222735c65d52b322347e4488bc660001d8f5b6
Arnaud Becheler <Arnaud.Becheler@egce.cnrs-gif.fr>
2017-08-09 15:53:40 +0200

    gdalwrapper test

---
+++ modules/geography/gdalcpp_test.cpp
@@ -0,0 +1,18 @@
+// Copyright 2016 Arnaud Becheler    <Arnaud.Becheler@egce.cnrs-gif.fr>
+
+/***********************************************************************                                                                         *
+* This program is free software; you can redistribute it and/or modify *
+* it under the terms of the GNU General Public License as published by *
+* the Free Software Foundation; either version 2 of the License, or    *
+* (at your option) any later version.                                  *
+*                                                                      *
+***************************************************************************/
+
+// compiles with g++ -o gdalcpp_test gdalcpp_test.cpp -std=c++14 -Wall -I/usr/include/gdal  -L/usr/lib/ -lgdal
+
+#include <assert.h>
+#include <iostream>
+
+#include "gdalcpp.h"
+
+int main(){}
