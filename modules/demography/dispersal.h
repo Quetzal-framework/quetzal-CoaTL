1a39880bce5d426421ecf4e5cbc08921c2e2d7cf
Arnaud Becheler <Arnaud.Becheler@egce.cnrs-gif.fr>
2018-06-12 16:38:15 +0200

    adding various nathans kernels

--- modules/demography/dispersal.h
+++ modules/demography/dispersal.h
@@ -17,57 +17,193 @@ namespace quetzal{
     namespace dispersal {
 
 //nathan et al 2012, table 15.1
-struct Logistic
-{
-  static double pdf(double r, double a, double b)
-  {
-    assert(a > 0 && b >2 && r >= 0);
-    return (b/(2*M_PI*(a*a)*std::tgamma(2/b)*std::tgamma(1-2/b)))*(1/(1+(std::pow(r,b)/(std::pow(a,b)))));
-  }
-};
-
 struct Gaussian
 {
-  static double pdf(double r, double a)
+  class param_type{
+    double _a;
+  public:
+    double a() const {return _a;}
+    void a(double value) { _a = value ;}
+  };
+
+  static double pdf(double r, param_type const& p)
   {
+    double a = p.a();
     assert(a > 0 && r >= 0);
     return 1/(M_PI*a*a) * std::exp(-(r*r)/(a*a)) ;
   }
 };
 
+struct Logistic
+{
+  class param_type{
+    double _a;
+    double _b;
+  public:
+    double a() const {return _a;}
+    double b() const {return _b;}
+    void a(double value) { _a = value;}
+    void b(double value) { _b = value;}
+  };
+
+  static double pdf(double r, param_type const& p)
+  {
+    double a = p.a();
+    double b = p.b();
+    assert(a > 0 && b >2 && r >= 0);
+    return (b/(2*M_PI*(a*a)*std::tgamma(2/b)*std::tgamma(1-2/b)))*(1/(1+(std::pow(r,b)/(std::pow(a,b)))));
+  }
+
+};
+
 struct NegativeExponential
 {
-  static double pdf(double r, double a)
+  class param_type{
+    double _a;
+  public:
+    double a() const {return _a;}
+    void a(double value) { _a = value;}
+  };
+
+  static double pdf(double r, param_type const& p)
   {
-    assert(a > 0 && r >= 0);
-    return  (1/2*M_PI*a*a)*std::exp(-r/a);
+    double a = p.a();
+    assert(a > 0 && r>= 0);
+    return 1/(2*M_PI*a*a) * std::exp(-r/a) ;
+  }
+
+};
+
+struct ExponentialPower
+{
+  class param_type{
+    double _a;
+    double _b;
+  public:
+    double a() const {return _a;}
+    double b() const {return _b;}
+    void a(double value) { _a = value;}
+    void b(double value) { _b = value;}
+  };
+
+  static double pdf(double r, param_type const& p)
+  {
+    double a = p.a();
+    double b = p.b();
+    assert(a > 0 && b > 0 && r >= 0);
+    return b/(2*M_PI*a*a*std::tgamma(2/b)) * std::exp(-std::pow(r,b)/std::pow(a,b));
+  }
+
+};
+
+struct 2Dt
+{
+  class param_type{
+    double _a;
+    double _b;
+  public:
+    double a() const {return _a;}
+    double b() const {return _b;}
+    void a(double value) { _a = value;}
+    void b(double value) { _b = value;}
+  };
+
+  static double pdf(double r, param_type const& p)
+  {
+    double a = p.a();
+    double b = p.b();
+    assert(a > 0 && b > 1 && r >= 0);
+    return (b-1)/(M_PI*a*a) * std::pow( (1 + (r*r)/(a*a)) , -b);
   }
 };
 
-template<typename Kernel, typename Distance, typename Space, typename... Args>
-quetzal::random::DiscreteDistribution<Space>
-make_dispersal_location_random_distribution(Space const& x, std::vector<Space> const& demes, Distance d, Args&&... args)
+struct InversePowerLaw
 {
-  std::vector<double> weights;
-  weights.reserve(demes.size());
-  for(auto const& it : demes)
-  {
-    weights.push_back(Kernel::pdf(d(x, it), std::forward<Args>(args)...));
-  }
-  return quetzal::random::DiscreteDistribution<Space>(demes, weights);
-}
+  class param_type{
+    double _a;
+    double _b;
+  public:
+    double a() const {return _a;}
+    double b() const {return _b;}
+    void a(double value) { _a = value;}
+    void b(double value) { _b = value;}
+  };
 
-template<typename Kernel, typename Distance, typename Space, typename... Args>
-auto make_dispersal_kernel(std::vector<Space> const& demes, Distance d, Args&&... args)
-{
-  assert(!demes.empty());
-  using law_type =  quetzal::random::DiscreteDistribution<Space>;
-  quetzal::random::TransitionKernel<law_type> kernel;
-  for(auto const& it : demes)
+  static double pdf(double r, param_type const& p)
   {
-    kernel.set(it, make_dispersal_location_random_distribution<Kernel>(it, demes, d, std::forward<Args>(args)...));
+    double a = p.a();
+    double b = p.b();
+    assert(a > 0 && b > 2 && r >= 0);
+    return (b-2)(b-1)/(2*M_PI*a*a) * std::pow( (1 + r/a) , -b);
   }
-  return kernel;
-}
+};
+
+struct UndefinedInversePowerLaw
+{
+  class param_type{
+    double _a;
+    double _b;
+  public:
+    double a() const {return _a;}
+    double b() const {return _b;}
+    void a(double value) { _a = value;}
+    void b(double value) { _b = value;}
+  };
+
+  static double pdf(double r, param_type const& p)
+  {
+    double a = p.a();
+    double b = p.b();
+    assert(a > 0 && b > 2 && r >= 0);
+    return std::pow( r/a , -b);
+  }
+};
+
+
+struct Lognormal
+{
+  class param_type{
+    double _a;
+    double _b;
+  public:
+    double a() const {return _a;}
+    double b() const {return _b;}
+    void a(double value) { _a = value;}
+    void b(double value) { _b = value;}
+  };
+
+  static double pdf(double r, param_type const& p)
+  {
+    double a = p.a();
+    double b = p.b();
+    assert(a > 0 && b > 0 && r >= 0);
+    return (1/(std::pow(2*M_PI, 3/2)*b*r*r)) * std::exp(- (std::log(r/a)*std::log(r/a) / (2*b*b) ) );
+  }
+};
+
+struct GaussianMixture
+{
+  class param_type{
+    double _a1;
+    double _a2;
+    double _p;
+  public:
+    double a1() const {return _a1;}
+    double a2() const {return _a2;}
+    double p() const {return _p;}
+    void a1(double value) { _a1 = value;}
+    void a2(double value) { _a2 = value;}
+    void p(double value) { _p = value;}
+  };
+
+  static double pdf(double r, param_type const& param)
+  {
+    double a1 = param.a1();
+    double a2 = param.a2();
+    double p = param.p();
+    assert(a1 > 0 && a2 > 0 && p > 0 && p < 1 && r >= 0);
+    return p/(M_PI*a1*a1) * std::exp(-(r*r)/(a1*a1)) + (1-p)/(M_PI*a2*a2) * std::exp(-(r*r)/(a2*a2)) ;
+  }
+};
 
 }}}
