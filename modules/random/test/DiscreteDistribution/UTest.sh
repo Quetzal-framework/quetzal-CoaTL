21849d5dc58c04995b036a98e5cf8274371e0825
Arnaud Becheler <Arnaud.Becheler@egce.cnrs-gif.fr>
2019-04-26 01:56:56 -0400

    utest ctest random

--- modules/random/test/DiscreteDistribution/UTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-g++ -o UTest -Wall -std=c++14 DiscreteDistribution_test.cpp
-./UTest > DiscreteDistribution_test.output
-rm UTest
-
-g++ -o UTest -Wall -std=c++14 initialization_test.cpp
-./UTest > initialization_test.output
-rm UTest
--- modules/random/test/TimeTransitionKernel/UTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-g++ -o UTest -Wall -std=c++14 TimeTransitionKernel_test.cpp
-./UTest > TimeTransitionKernel_test.output
-rm UTest
-
-g++ -o UTest -Wall -std=c++14 initialization_test.cpp
-./UTest > initialization_test.output
-rm UTest
