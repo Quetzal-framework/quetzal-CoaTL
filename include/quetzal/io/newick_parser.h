bf010fc67a808d11fa01a93e62132b63a909abc7
arnaud <arnaud@irreverentlabs.com>
2022-11-15 15:53:22 -0800

    test: basic newick parsing works

--- include/quetzal/io/newick_parser.h
+++ include/quetzal/io/newick_parser.h
@@ -2,32 +2,44 @@
 #define __NEWICK_PARSER_H_INCLUDED__
 
 #include <boost/spirit/home/x3.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/io.hpp>
 
+namespace quetzal::newick::ast
+{
+  ///////////////////////////////////////////////////////////////////////////
+  //  Our node struct
+  ///////////////////////////////////////////////////////////////////////////
+  struct node
+  {
+      std::string name;
+      double length;
+  };
+  using boost::fusion::operator<<;
+}
+
+// Tell fusion about our node struct to make it a first-class citizen.
+BOOST_FUSION_ADAPT_STRUCT(quetzal::newick::ast::node, name, length)
+
+///////////////////////////////////////////////////////////////////////////////
+//  Our node parser
+///////////////////////////////////////////////////////////////////////////////
 namespace quetzal::newick::parser
 {
   namespace x3 = boost::spirit::x3;
 
-  using x3::alpha;
-  using x3::alnum;
-  using x3::double_;
-  using x3::rule;
-  using x3::phrase_parse;
-  using x3::space;
+  x3::rule<struct branch> branch{"branch"};
 
-  rule<struct branch> branch{"branch"};
-  rule<struct branch_set> branch_set{"branch_set"};
+  auto name     = x3::lexeme[x3::alpha >> *x3::alnum]; // to be improved later
+  auto length   = ':' >> x3::double_;
+  auto leaf     = -name;
+  auto internal = '(' >> (branch % ',') >> ')' >> -name;
+  auto subtree  = internal | leaf;
+  auto tree     = x3::skip(x3::blank)[subtree >> -length >> ';' >> x3::eoi];
 
-  auto name    = alpha >> *alnum;
-  auto length  = ':' > double_;
-  auto leaf    = -name;
-  auto internal= '(' >> branch_set >> ')' >> -name;
-  auto subtree = leaf | internal;
-  auto tree    = subtree >> ';';
+  auto branch_def = subtree >> -length;
 
-  auto const branch_def = subtree >> length;
-  auto const branch_set_def = branch | branch >> ',' >> branch_set;
-
-  BOOST_SPIRIT_DEFINE(branch, branch_set);
+  BOOST_SPIRIT_DEFINE(branch);
 }
 
 #endif
--- test/unit_test/newick_parser_test.cpp
+++ test/unit_test/newick_parser_test.cpp
@@ -13,17 +13,139 @@
 #include <boost/test/unit_test.hpp>
 namespace utf = boost::unit_test;
 
+#define BOOST_SPIRIT_X3_DEBUG
+
 #include <boost/spirit/home/x3.hpp>
 #include "quetzal/io/newick_parser.h"
+#include <iomanip>
 
 namespace x3 = boost::spirit::x3;
 
+
 BOOST_AUTO_TEST_SUITE( newick_parser )
+
+BOOST_AUTO_TEST_CASE(name_grammar)
+{
+  std::vector<std::string> inputs =
+  {
+    "a34",
+    "A856BC",
+    "k56aH"
+  };
+
+  for(const auto& input : inputs)
+  {
+    auto iter = input.begin();
+    auto iter_end = input.end();
+    bool r = phrase_parse(iter, iter_end, quetzal::newick::parser::name, x3::space );
+    BOOST_CHECK(r && iter == iter_end);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(length_grammar)
+{
+  std::vector<std::string> inputs =
+  {
+    ":1",
+    ":999",
+    ":0.001"
+  };
+
+  for(const auto& input : inputs)
+  {
+    auto iter = input.begin();
+    auto iter_end = input.end();
+    bool r = phrase_parse(iter, iter_end, quetzal::newick::parser::length, x3::space );
+    BOOST_CHECK(r && iter == iter_end);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(leaf_grammar)
+{
+  std::vector<std::string> inputs =
+  {
+    "",
+    "A",
+    "B897kj"
+  };
+
+  for(const auto& input : inputs)
+  {
+    auto iter = input.begin();
+    auto iter_end = input.end();
+    bool r = phrase_parse(iter, iter_end, quetzal::newick::parser::leaf, x3::space );
+    BOOST_CHECK(r && iter == iter_end);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(branch_grammar)
+{
+  std::vector<std::string> inputs =
+  {
+    "",
+    "A",
+    "A38",
+    "A38:10.0"
+  };
+
+  for(const auto& input : inputs)
+  {
+    auto iter = input.begin();
+    auto iter_end = input.end();
+    bool r = phrase_parse(iter, iter_end, quetzal::newick::parser::branch, x3::space );
+    BOOST_CHECK(r && iter == iter_end);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(internal_grammar)
+{
+  std::vector<std::string> inputs =
+  {
+    "(,)",
+    "(A,B)F",
+    "(A:10,B:10)F"
+  };
+
+  for(const auto& input : inputs)
+  {
+    auto iter = input.begin();
+    auto iter_end = input.end();
+    bool r = phrase_parse(iter, iter_end, quetzal::newick::parser::internal, x3::space );
+    BOOST_CHECK(r && iter == iter_end);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(subtree_grammar)
+{
+  std::vector<std::string> inputs =
+  {
+    "leaf",
+    "(in,ternal)",
+    "(,,(,))",
+    "(A,B,(C,D))",
+    "(A,B,(C,D)E)F",
+    "(:0.1,:0.2,(:0.3,:0.4):0.5)",
+    "(:0.1,:0.2,(:0.3,:0.4):0.5)",
+    "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5)",
+    "(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F",
+    "((B:0.2,(C:0.3,D:0.4)E:0.5)F:0.1)A"
+  };
+
+  for(const auto& input : inputs)
+  {
+    auto iter = input.begin();
+    auto iter_end = input.end();
+    bool r = phrase_parse(iter, iter_end, quetzal::newick::parser::subtree, x3::space );
+    BOOST_CHECK(r && iter == iter_end);
+  }
+}
 
 BOOST_AUTO_TEST_CASE(full_grammar)
 {
   std::vector<std::string> inputs =
   {
+    ";",
+    "(,);",
     "(,,(,));",
     "(A,B,(C,D));",
     "(A,B,(C,D)E)F;",
@@ -39,7 +161,9 @@ BOOST_AUTO_TEST_CASE(full_grammar)
     auto iter = input.begin();
     auto iter_end = input.end();
     bool r = phrase_parse(iter, iter_end, quetzal::newick::parser::tree, x3::space );
-    BOOST_CHECK(r);
+    std::cout << quoted(input) << " \t-> " << std::boolalpha
+              << parse(begin(input), end(input), quetzal::newick::parser::tree) << std::endl;
+    BOOST_CHECK(r && iter == iter_end);
   }
 }
 
