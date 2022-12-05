e79b425b0398081374d94ec820ae9acfcce727b2
arnaud <arnaud@irreverentlabs.com>
2022-12-04 18:01:45 -0800

    doc: recursive functor ok for newick ast populate

--- example/newick_parser.cpp
+++ example/newick_parser.cpp
@@ -1,5 +1,8 @@
-#include "quetzal/io.hpp"
-#include <iostream>
+#include "quetzal/io.hpp"          // parsers and generators
+#include "quetzal/coalescence.hpp" // Tree data structure
+#include <iostream>                // std::cout
+#include <functional>              // std::function
+#include <utility>                 // std::pair
 
 // Define a namespace alias to shorten notation
 namespace newick = quetzal::format::newick;
@@ -18,19 +21,42 @@ int main()
   // Loop over all Newick strings cases
   for(const auto& input : cases)
   {
-    // Initialize the abstract syntax tree
-    newick::ast::node root;
+    // We define a shorter type alias
+    using ast_type = newick::ast::node;
 
-    // parse the input into the AST
-    auto ok = quetzal::parse(begin(input), end(input), newick::parser::tree, root);
+    // We initialize the root of Abstract Syntax Tree (AST)
+    ast_type ast_root;
 
-    // Process the AST using its << operator
+    // We parse the input string into the AST
+    auto ok = quetzal::parse(begin(input), end(input), newick::parser::tree, ast_root);
+
+    // We check by printing the AST if the string was successfully parsed
     std::cout << quoted(input) << "\t " << (ok ? "Parsed" : "Failed");
-    if (ok) std::cout << "\n" << root << std::endl;
+    if (ok) std::cout << "\n" << ast_root << std::endl;
 
-    // You can also manually explore the recursive AST
-    // for (auto& child : root.children)
-    //   std::cout << root.name << " <-> " << child.name << ":" << root.length << std::endl;
+    // You can now iterate over the AST to populate a data structure of your choice
+
+    // Here we illustrate the case with a Quetzal Tree
+    using quetzal::coalescence::container::Tree;
+    // each Tree node is defined to contain a cell with 2 fields (name and branch length)
+    using cell_type = std::pair<std::string, double>;
+    // We define another short type alias
+    using tree_type = Tree<cell_type>;
+
+    // We initialize the root of the Quetzal Tree
+    tree_type tree_root({ast_root.name, ast_root.distance_to_parent});
+
+    // We declare a recursive function object to update the Tree reading from the AST
+    std::function<void(tree_type&, const ast_type&)> populate =
+    [&](auto& tree, const auto& ast){
+      for(const auto& ast_child : ast.children){
+        auto subtree = tree.add_child({ast_child.name, ast_child.distance_to_parent});
+        populate(subtree, ast_child);
+      }
+    };
+
+    // Now we simply call the recursive function to populate from the root
+    populate(tree_root, ast_root);
   }
 
   return 0;
